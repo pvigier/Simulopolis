cf5382733672a8e3c686a99cdba4b2486fd4d811
Pierre Vigier <pierre.vigier@ymail.com>
2018-02-23 22:52:28 +0100

    Add variants to buildings

--- src/city/Building.cpp
+++ src/city/Building.cpp
@@ -19,8 +19,19 @@ std::unique_ptr<Tile> Building::clone() const
 bool Building::updateVariant(Tile* neighbors[3][3])
 {
     sf::IntRect rect(0, 0, 132, 165);
+
+    if (neighbors[0][1]->getType() == Type::ROAD)
+        rect.left = 264;
+    else if (neighbors[1][2]->getType() == Type::ROAD)
+        rect.left = 132;
+    else if (neighbors[1][0]->getType() == Type::ROAD)
+        rect.left = 396;
+
+    if (mSprite.getTextureRect() == rect)
+        return false;
+
     mSprite.setTextureRect(rect);
-    return false;
+    return true;
 }
 
 bool Building::isBuilding() const
