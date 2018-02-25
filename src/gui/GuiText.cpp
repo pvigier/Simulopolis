7532fe475bd033ed8fbc801f9d6d66d5b92fd7f3
Pierre Vigier <pierre.vigier@ymail.com>
2018-02-25 18:03:58 +0100

    Add GuiText

---
+++ src/gui/GuiText.cpp
@@ -0,0 +1,41 @@
+#include "GuiText.h"
+
+GuiText::GuiText(const std::string& text, unsigned int characterSize, const GuiStyle& style) :
+    mText(text, *style.font, characterSize)
+{
+    mText.setFillColor(style.textColor);
+    computeSize();
+}
+
+GuiText::~GuiText()
+{
+    //dtor
+}
+
+void GuiText::setPosition(sf::Vector2f position)
+{
+    sf::Vector2f offset(sf::Vector2f(mText.getGlobalBounds().left, mText.getGlobalBounds().top) - mPosition);
+    GuiWidget::setPosition(position);
+    mText.setPosition(position - offset);
+}
+
+void GuiText::setCharacterSize(unsigned int characterSize)
+{
+    mText.setCharacterSize(characterSize);
+    computeSize();
+}
+
+void GuiText::setText(const std::string& text)
+{
+    mText.setString(text);
+}
+
+void GuiText::render(sf::RenderTarget& target, sf::RenderStates states) const
+{
+    target.draw(mText);
+}
+
+void GuiText::computeSize()
+{
+    mSize = sf::Vector2f(mText.getGlobalBounds().width, mText.getGlobalBounds().height);
+}
---
+++ src/gui/GuiText.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "SFML/Graphics.hpp"
+#include "gui/GuiWidget.h"
+#include "gui/GuiStyle.h"
+
+class GuiText : public GuiWidget
+{
+public:
+    GuiText(const std::string& text, unsigned int characterSize, const GuiStyle& style);
+    virtual ~GuiText();
+
+    virtual void setPosition(sf::Vector2f position) override;
+    virtual void setCharacterSize(unsigned int characterSize);
+    void setText(const std::string& text);
+
+protected:
+    virtual void render(sf::RenderTarget& target, sf::RenderStates states) const override;
+
+private:
+    sf::Text mText;
+
+    void computeSize();
+};
