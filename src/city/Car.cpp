d64c6c56da52fa7e335e91eeb64fb011472d06cd
Pierre Vigier <pierre.vigier@ymail.com>
2018-04-16 18:50:42 +0200

    Improve the behavior of cars

--- src/city/Car.cpp
+++ src/city/Car.cpp
@@ -2,10 +2,11 @@
 
 TextureManager* Car::sTextureManager = nullptr;
 
-Car::Car() : mKinematic(1.0f, 150.0f, 600.0f), mSteering(mKinematic)
+Car::Car() : mKinematic(1.0f, 150.0f, 1200.0f), mSteering(mKinematic)
 {
     mSprite.setTexture(sTextureManager->getTexture("car"));
     mSprite.setTextureRect(sf::IntRect(0, 0, WIDTH, HEIGHT));
+    mSprite.setOrigin(sf::Vector2f(WIDTH * 0.5f, HEIGHT * 0.5f));
 
     mSteering.setSeekDistance(20.0f);
     mSteering.setArriveDistance(100.0f);
@@ -35,9 +36,6 @@ void Car::update(float dt)
         angle = -angle;
     int i = static_cast<int>((angle + 22.5f) / 45.0f) % 8;
     mSprite.setTextureRect(sf::IntRect(i * WIDTH, 0, WIDTH, HEIGHT));
-    std::cout << angle << std::endl;
-    std::cout << i << std::endl;
-    //mShape.setRotation();
 }
 
 
