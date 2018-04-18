563841d26bcba2e003b1e539082d8fd898d13bb2
Pierre Vigier <pierre.vigier@ymail.com>
2018-04-18 19:02:19 +0200

    Add XmlDocument

---
+++ src/resource/XmlDocument.cpp
@@ -0,0 +1,33 @@
+#include "XmlDocument.h"
+
+XmlDocument::XmlDocument(std::string name, PropertyList attributes, std::vector<XmlDocument> children) :
+    mName(std::move(name)), mAttributes(std::move(attributes)), mChildren(std::move(children))
+{
+    for (std::size_t i = 0; i < mChildren.size(); ++i)
+        mChildrenByName[mChildren[i].getName()].push_back(i);
+}
+
+const std::string& XmlDocument::getName() const
+{
+    return mName;
+}
+
+const PropertyList& XmlDocument::getAttributes() const
+{
+    return mAttributes;
+}
+
+const std::vector<XmlDocument>& XmlDocument::getChildren() const
+{
+    return mChildren;
+}
+
+const std::vector<std::size_t>& XmlDocument::getChildrenByName(const std::string& name) const
+{
+    return mChildrenByName.at(name);
+}
+
+const XmlDocument& XmlDocument::getFirstChildByName(const std::string& name) const
+{
+    return mChildren.at(mChildrenByName.at(name).front());
+}
---
+++ src/resource/XmlDocument.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <vector>
+#include <unordered_map>
+#include "PropertyList.h"
+
+class XmlDocument
+{
+public:
+    XmlDocument(std::string name, PropertyList attributes, std::vector<XmlDocument> children);
+
+    const std::string& getName() const;
+    const PropertyList& getAttributes() const;
+    const std::vector<XmlDocument>& getChildren() const;
+    const std::vector<std::size_t>& getChildrenByName(const std::string& name) const;
+    const XmlDocument& getFirstChildByName(const std::string& name) const;
+
+private:
+    std::string mName;
+    PropertyList mAttributes;
+    std::vector<XmlDocument> mChildren;
+    std::unordered_map<std::string, std::vector<std::size_t>> mChildrenByName;
+};
