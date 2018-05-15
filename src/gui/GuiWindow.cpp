e10bbca0a149e55e4f23e6a406973fbba83017a3
Pierre Vigier <pierre.vigier@ymail.com>
2018-05-15 18:16:53 +0200

    Make the windows prettier

--- src/gui/GuiWindow.cpp
+++ src/gui/GuiWindow.cpp
@@ -4,18 +4,18 @@
 GuiWindow::GuiWindow(sf::Vector2f size, const std::string& title, const XmlDocument* style) :
     mStyle(style), mOnMove(false)
 {
+    applyStyle();
     setSize(size);
     setTitle(title);
-    applyStyle();
 }
 
 GuiWindow::GuiWindow(const PropertyList& properties) :
     GuiWidget(properties), mStyle(properties.get<const XmlDocument*>("style"))
 {
+    applyStyle();
     setSize(mSize);
     setPosition(mPosition);
     setTitle(properties.get("title"));
-    applyStyle();
 }
 
 void GuiWindow::setPosition(sf::Vector2f position)
@@ -23,13 +23,14 @@ void GuiWindow::setPosition(sf::Vector2f position)
     GuiWidget::setPosition(position);
     mBody.setPosition(position);
     mBar.setPosition(position);
+    sf::Vector2f barSize = sf::Vector2f(mBar.getGlobalBounds().width, mBar.getGlobalBounds().height);
     // Close button
     sf::Vector2f closeButtonSize = sf::Vector2f(mCloseButton.getGlobalBounds().width, mCloseButton.getGlobalBounds().height);
-    mCloseButton.setPosition(position + sf::Vector2f(mSize.x - closeButtonSize.x * 1.5f, -mBar.getSize().y * 0.5f - closeButtonSize.y * 0.5f));
+    mCloseButton.setPosition(position + sf::Vector2f(mSize.x - closeButtonSize.x * 1.5f, -barSize.y * 0.5f - closeButtonSize.y * 0.5f));
     // Title
     sf::Vector2f offset(sf::Vector2f(mTitle.getGlobalBounds().left, mTitle.getGlobalBounds().top) - mTitle.getPosition());
     sf::Vector2f titleSize = sf::Vector2f(mTitle.getGlobalBounds().width, mTitle.getGlobalBounds().height);
-    sf::Vector2i titlePosition(position + sf::Vector2f(mSize.x * 0.5f, -mBar.getSize().y * 0.5f) - offset - titleSize * 0.5f);
+    sf::Vector2i titlePosition(position + sf::Vector2f(mSize.x * 0.5f, -barSize.y * 0.5f) - offset - titleSize * 0.5f);
     mTitle.setPosition(sf::Vector2f(titlePosition));
 }
 
@@ -95,13 +96,13 @@ void GuiWindow::render(sf::RenderTarget& target, sf::RenderStates states) const
 void GuiWindow::applyStyle()
 {
     // Body
-    mBody.setOutlineThickness(-mStyle->getFirstChildByName("border").getAttributes().get<int>("size"));
+    mBody.setOutlineThickness(mStyle->getFirstChildByName("border").getAttributes().get<int>("size"));
     mBody.setFillColor(mStyle->getFirstChildByName("body").getAttributes().get<sf::Color>("color"));
     mBody.setOutlineColor(mStyle->getFirstChildByName("border").getAttributes().get<sf::Color>("color"));
     // Bar
-    mBar.setOrigin(0.0f, mStyle->getFirstChildByName("bar").getAttributes().get<float>("height") -
+    mBar.setOrigin(0.0f, mStyle->getFirstChildByName("bar").getAttributes().get<float>("height") +
         mStyle->getFirstChildByName("border").getAttributes().get<int>("size"));
-    mBar.setOutlineThickness(-mStyle->getFirstChildByName("border").getAttributes().get<int>("size"));
+    mBar.setOutlineThickness(mStyle->getFirstChildByName("border").getAttributes().get<int>("size"));
     mBar.setFillColor(mStyle->getFirstChildByName("bar").getAttributes().get<sf::Color>("color"));
     mBar.setOutlineColor(mStyle->getFirstChildByName("border").getAttributes().get<sf::Color>("color"));
     // Close button
