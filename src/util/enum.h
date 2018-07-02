e2f98c5a76fcd3d7bc2c696393a7ffb4394c76ed
Pierre Vigier <pierre.vigier@ymail.com>
2018-07-02 15:50:13 +0200

    Improve enum.h

--- src/util/enum.h
+++ src/util/enum.h
@@ -7,6 +7,19 @@ struct EnableBitMaskOperators
 };
 
 template<typename E>
+typename std::enable_if<EnableBitMaskOperators<E>::enable, E>::type constexpr none = static_cast<E>(0);
+
+template<typename E>
+typename std::enable_if<EnableBitMaskOperators<E>::enable, E>::type constexpr all = static_cast<E>(~0);
+
+template<typename E>
+typename std::enable_if<EnableBitMaskOperators<E>::enable, E>::type operator~(E e)
+{
+    using underlying = typename std::underlying_type<E>::type;
+    return static_cast<E>(~static_cast<underlying>(e));
+}
+
+template<typename E>
 typename std::enable_if<EnableBitMaskOperators<E>::enable, E>::type operator|(E lhs, E rhs)
 {
     using underlying = typename std::underlying_type<E>::type;
