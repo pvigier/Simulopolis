4c3589ae1d7c5e0cc51592b613e120c581d7ecfa
Pierre Vigier <pierre.vigier@ymail.com>
2018-07-20 17:32:51 +0200

    Add GoalThink

---
+++ src/ai/GoalThink.cpp
@@ -0,0 +1,52 @@
+#include "GoalThink.h"
+
+GoalThink::GoalThink(Person* owner) : Goal(owner)
+{
+    //ctor
+}
+
+GoalThink::~GoalThink()
+{
+    //dtor
+}
+
+void GoalThink::activate()
+{
+    mState = State::ACTIVE;
+    // Add subgoals
+    clearSubgoals();
+    arbitrate();
+}
+
+Goal::State GoalThink::process()
+{
+    activateIfInactive();
+
+    mState = processSubgoals();
+    if (mState == State::COMPLETED || mState == State::FAILED)
+        mState = State::INACTIVE;
+    return mState;
+}
+
+void GoalThink::terminate()
+{
+
+}
+
+void GoalThink::arbitrate()
+{
+    float maxDesirability = std::numeric_limits<float>::min();
+    const GoalEvaluator* bestEvaluator = nullptr;
+    for (const std::unique_ptr<GoalEvaluator>& evaluator : mEvaluators)
+    {
+        float desirability = evaluator->computeDesirability(mOwner);
+        if (desirability > maxDesirability)
+        {
+            maxDesirability = desirability;
+            bestEvaluator = evaluator.get();
+        }
+    }
+
+    if (bestEvaluator)
+        bestEvaluator->setGoal(mOwner);
+}
---
+++ src/ai/GoalThink.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <vector>
+#include "ai/Goal.h"
+#include "ai/GoalEvaluator.h"
+
+class GoalThink : public Goal
+{
+public:
+    GoalThink(Person* owner);
+    virtual ~GoalThink();
+
+    virtual void activate() override;
+    virtual State process() override;
+    virtual void terminate() override;
+
+private:
+    std::vector<std::unique_ptr<GoalEvaluator>> mEvaluators;
+
+    void arbitrate();
+};
