df57b65abcef97b6c9fbd1adfb9654271c586219
Pierre Vigier <pierre.vigier@ymail.com>
2018-08-17 12:21:50 +0200

    Add strong_typedef.h

---
+++ src/util/strong_typedef.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#define STRONG_TYPEDEF(Type, Handle)       \
+class Handle                               \
+{                                          \
+public:                                    \
+    Handle()                               \
+    {                                      \
+                                           \
+    }                                      \
+                                           \
+    explicit Handle(const Type& t) : mT(t) \
+    {                                      \
+                                           \
+    }                                      \
+                                           \
+    Handle& operator=(const Type& t)       \
+    {                                      \
+        mT = t;                            \
+        return *this;                      \
+    }                                      \
+                                           \
+    operator Type&()                       \
+    {                                      \
+        return mT;                         \
+    }                                      \
+                                           \
+    bool operator<(const Handle& h) const  \
+    {                                      \
+        return mT < h.mT;                  \
+    }                                      \
+                                           \
+private:                                   \
+    Type mT;                               \
+};
