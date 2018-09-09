4a9a3bf914e65e79cf744eadd93785e504d0c052
Pierre Vigier <pierre.vigier@ymail.com>
2018-09-09 13:14:59 +0200

    Update the code so that it compiles with mingw

--- src/util/common.h
+++ src/util/common.h
@@ -10,7 +10,7 @@ constexpr float FLOAT_MAX = std::numeric_limits<float>::max();
 constexpr float FLOAT_INFINITY = std::numeric_limits<float>::infinity();
 
 // Constants
-constexpr float PI = M_PI;
+constexpr float PI = 3.1415926535f;
 constexpr float INV_PI = 1.0f / PI;
 constexpr float SQRT_2 = std::sqrt(2.0f);
 
