c6b56f913f15db0a5b18abc0e70bf6d19a4e36b9
Pierre Vigier <pierre.vigier@ymail.com>
2018-10-14 21:44:01 +0200

    Add a join function

--- src/util/string.h
+++ src/util/string.h
@@ -4,3 +4,15 @@
 #include <string>
 
 std::vector<std::string> split(const std::string& string, char delimiter);
+
+template <typename Container, typename Separator>
+std::string join(const Container& tokens, Separator separator)
+{
+    if (tokens.empty())
+        return "";
+    auto it = std::begin(tokens);
+    std::string result = *it;
+    for (++it; it != std::end(tokens); ++it)
+        result += separator + *it;
+    return result;
+}
