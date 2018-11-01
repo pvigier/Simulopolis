3f68b4b926ea0ca0956eef2fd466f90f35dd2858
Pierre Vigier <pierre.vigier@ymail.com>
2018-11-01 13:19:33 +0100

    Remove the last new in the code

--- src/util/format.h
+++ src/util/format.h
@@ -21,11 +21,11 @@
 #include <string>
 #include <cstdio>
 
-template<typename ... Args>
+template<typename... Args>
 std::string format(const std::string& format, Args... args)
 {
     std::size_t size = snprintf(nullptr, 0, format.c_str(), args...) + 1;
-    std::unique_ptr<char[]> buffer(new char[size]);
+    std::unique_ptr<char[]> buffer = std::make_unique<char[]>(size);
     snprintf(buffer.get(), size, format.c_str(), args...);
     return std::string(buffer.get(), buffer.get() + size - 1);
 }
