87fb8d48ca24ab477ee9415d63cbf30f28eb3c68
Pierre Vigier <pierre.vigier@ymail.com>
2018-11-01 16:37:43 +0100

    Add Trie

---
+++ src/util/Trie.h
@@ -0,0 +1,88 @@
+#pragma once
+
+#include <memory>
+#include <unordered_map>
+#include <ostream>
+
+template <typename Char>
+class Trie
+{
+public:
+    Trie() : mRoot(std::make_unique<Node>())
+    {
+
+    }
+
+    template <typename String>
+    void insert(String&& string)
+    {
+        Node* node = mRoot.get();
+        for (auto&& c : string)
+        {
+            if (node->children.find(c) == node->children.end())
+                node->children[c] = std::make_unique<Node>();
+            node = node->children[c].get();
+        }
+        node->end = true;
+    }
+
+    // Overload for C strings
+    template <typename T>
+    void insert(T* string)
+    {
+        insert(std::basic_string<Char>(string));
+    }
+
+    template <typename String>
+    bool contains(String&& string) const    
+    {
+        Node* node = mRoot.get();
+        for (auto&& c : string)
+        {
+            if (node->children.find(c) == node->children.end())
+                return false;
+            node = node->children[c].get();
+        }
+        return node->end;
+
+    }
+
+    // Overload for C strings
+    template <typename T>
+    bool contains(T* string) const
+    {
+        return contains(std::basic_string<Char>(string));
+    }
+
+    std::ostream& print(std::ostream& os) const 
+    {
+        os << "root ";
+        return print(os, mRoot.get(), "\t");
+    }
+
+private:
+    struct Node
+    {
+        std::unordered_map<Char, std::unique_ptr<Node>> children;        
+        bool end = false;
+    };
+    
+    std::unique_ptr<Node> mRoot;
+
+    std::ostream& print(std::ostream& os, const Node* node, std::string tabs) const
+    {
+        os << node->end << '\n';
+        for (const auto& kv : node->children)
+        {
+            os << tabs << kv.first << ' '; 
+            print(os, kv.second.get(), tabs + '\t');
+        }
+        return os;
+    }
+};
+
+template <typename Char>
+std::ostream& operator<<(std::ostream& os, const Trie<Char>& trie)
+{
+    return trie.print(os);
+}
