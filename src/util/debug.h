f5c66c46cae10fbab83fb341d96f89d9a5bd7442
Pierre Vigier <pierre.vigier@ymail.com>
2018-11-06 23:16:22 +0100

    Improve error logging

--- src/util/debug.h
+++ src/util/debug.h
@@ -19,10 +19,12 @@
 
 #define DEBUG_BUILD // Comment for release build
 
+// Some debug macros
+
 #ifdef DEBUG_BUILD
     #include <iostream>
-    #define DEBUG(msg) do { std::cout << msg << std::flush; } while (0)
-    #define DEBUG_IF(cond, msg) do { if (cond) std::cout << msg << std::flush; } while (0)
+    #define DEBUG(msg) do { std::cerr << msg; } while (0)
+    #define DEBUG_IF(cond, msg) do { if (cond) std::cerr << msg; } while (0)
 #else
     #define DEBUG(msg) do {} while (0)
     #define DEBUG_IF(cond, msg) do {} while (0)
